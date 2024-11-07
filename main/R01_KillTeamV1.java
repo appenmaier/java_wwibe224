07da9a95bafb640b6bb49c577cca4fedd05ceaf5
Daniel Appenmaier <daniel.appenmaier@gmail.com>
2024-11-07 08:35:20 +0100

    Update Comments

--- main/R01_KillTeamV1.java
+++ main/R01_KillTeamV1.java
@@ -57,17 +57,19 @@ public class R01_KillTeamV1 {
     healthPointsOfFighter2 = myScanner.nextInt();
 
     /* Spielablauf */
-    // Runde 1, Zug 1
+    // Zug 1 - Ausgabe
     System.out.println();
-    System.out.println("*----------------*");
-    System.out.println("* Runde 1, Zug 1 *");
-    System.out.println("*----------------*");
+    System.out.println("*-------*");
+    System.out.println("* Zug 1 *");
+    System.out.println("*-------*");
     System.out.println("Kämpfer von " + nameOfPlayer1 + ": " + nameOfFighter1 + " ("
         + healthPointsOfFighter1 + " LP)");
     System.out.println("Kämpfer von " + nameOfPlayer2 + ": " + nameOfFighter2 + " ("
         + healthPointsOfFighter2 + " LP)");
     System.out.println();
     System.out.println(nameOfFighter1 + " greift " + nameOfFighter2 + " an.");
+
+    // Zug 1- Treffer
     valueOfDice1 = myRandom.nextInt(1, 7);
     valueOfDice2 = myRandom.nextInt(1, 7);
     valueOfDice3 = myRandom.nextInt(1, 7);
@@ -76,6 +78,8 @@ public class R01_KillTeamV1 {
         + valueOfDice3 + " und " + valueOfDice4 + ".");
     int totalHits = valueOfDice1 + valueOfDice2 + valueOfDice3 + valueOfDice4;
     System.out.println(nameOfFighter1 + " erzielt " + totalHits + " Treffer.");
+
+    // Zug 1 - Blocks
     valueOfDice5 = myRandom.nextInt(1, 7);
     valueOfDice6 = myRandom.nextInt(1, 7);
     valueOfDice7 = myRandom.nextInt(1, 7);
@@ -83,23 +87,27 @@ public class R01_KillTeamV1 {
     System.out.println(nameOfFighter2 + " würfelt " + valueOfDice5 + ", " + valueOfDice6 + " und "
         + valueOfDice7 + ".");
     System.out.println(nameOfFighter2 + " erzielt " + totalBlocks + " Blocks.");
+
+    // Zug 1 - Schaden
     int damage = totalHits - totalBlocks;
     healthPointsOfFighter2 -= damage;
     System.out.println(nameOfFighter2 + " erleidet " + damage + " Schaden und hat noch "
         + healthPointsOfFighter2 + " LP.");
     System.out.println();
 
-    // Runde 1, Zug 2
+    // Zug 2 - Ausgabe
     System.out.println();
-    System.out.println("*----------------*");
-    System.out.println("* Runde 1, Zug 2 *");
-    System.out.println("*----------------*");
+    System.out.println("*-------*");
+    System.out.println("* Zug 2 *");
+    System.out.println("*-------*");
     System.out.println("Kämpfer von " + nameOfPlayer1 + ": " + nameOfFighter1 + " ("
         + healthPointsOfFighter1 + " LP)");
     System.out.println("Kämpfer von " + nameOfPlayer2 + ": " + nameOfFighter2 + " ("
         + healthPointsOfFighter2 + " LP)");
     System.out.println();
     System.out.println(nameOfFighter2 + " greift " + nameOfFighter1 + " an.");
+
+    // Zug 2 - Treffer
     valueOfDice1 = myRandom.nextInt(1, 7);
     valueOfDice2 = myRandom.nextInt(1, 7);
     valueOfDice3 = myRandom.nextInt(1, 7);
@@ -108,6 +116,8 @@ public class R01_KillTeamV1 {
         + valueOfDice3 + " und " + valueOfDice4 + ".");
     totalHits = valueOfDice1 + valueOfDice2 + valueOfDice3 + valueOfDice4;
     System.out.println(nameOfFighter2 + " erzielt " + totalHits + " Treffer.");
+
+    // Zug 2 - Blocks
     valueOfDice5 = myRandom.nextInt(1, 7);
     valueOfDice6 = myRandom.nextInt(1, 7);
     valueOfDice7 = myRandom.nextInt(1, 7);
@@ -115,6 +125,8 @@ public class R01_KillTeamV1 {
     System.out.println(nameOfFighter1 + " würfelt " + valueOfDice5 + ", " + valueOfDice6 + " und "
         + valueOfDice7 + ".");
     System.out.println(nameOfFighter1 + " erzielt " + totalBlocks + " Blocks.");
+
+    // Zug 2 - Schaden
     damage = totalHits - totalBlocks;
     healthPointsOfFighter1 -= damage;
     System.out.println(nameOfFighter1 + " erleidet " + damage + " Schaden und hat noch "
