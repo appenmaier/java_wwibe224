8b1edfa1a69abb3df09a0cb2e226bb8c11e91627
Daniel Appenmaier <daniel.appenmaier@gmail.com>
2025-04-10 12:59:56 +0200

    Add Demo 29

---
+++ src/main/java/model/MovieByRatingDescendingComparator.java
@@ -0,0 +1,19 @@
+package model;
+
+import java.util.Comparator;
+
+/**
+ * MovieByRatingDescendingComparator
+ *
+ * @author Daniel Appenmaier
+ * @version 1.0
+ *
+ */
+public class MovieByRatingDescendingComparator implements Comparator<Movie> {
+
+   @Override
+   public int compare(Movie movie1, Movie movie2) {
+      return Double.compare(movie2.getRating(), movie1.getRating());
+   }
+
+}
